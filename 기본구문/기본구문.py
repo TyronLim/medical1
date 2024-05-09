eadedb4a20340d953a6ec11f7a874ff1d023656a
TyronLim <ltk6533@naver.com>
2024-05-09 19:12:47 +0900

    pandas

--- 기본구문/기본구문.py
+++ 기본구문/기본구문.py
@@ -6,6 +6,37 @@ df.to_csv('score.txt',encoding='utf-8')
 df.to_excel('score.xlsx')
 
 
+#    --------------행----------------------------둘 다---------------------------열---------------
+# df            df[0:1]                                                     '열이름'                    
+# df        ['index':'index']                                       
+#  
+# df.loc        ['index']                                               
+# df.loc    ['index':'index']                                       
+# df.loc   [['index','index']]                  
+# df.loc                                   ['index','열이름']                         
+# df.loc                         [['index','index'],['열이름','열이름']]                  
+# 
+# df.iloc       [행주소]
+# df.iloc    [행주소:행주소]
+# df.iloc                                       [0,0]
+# df.iloc                                     [0:3,0:3]
+# df.iloc                                    [0:3,[0,1]]
+# df.iloc                                   [[0,2],[0,2]]
+# df.iloc                                    [[0,3],0:2]
+
+df.columns[0]   # '국어'
+
+
+df.drop(columns=['합계'],inplace=True)
+df.drop(index='1번',inplace=True)
+
+df['item_price'] = df['item_price'].str.replace('$','').astype(float)       # 타입 변경, 달라 삭제
+
+df.dropna(axis='columns',how='any',inplace=True)        ## 행이냐 열이냐 선택해서 NaN 삭제
+
+bSet = list(zip('name','births','custom'))    ## zip
+list(zip('name','births'))                  ## zip
+
 str.slice(1,3)
 
 df.loc['1번',['이름','키']]
@@ -16,6 +47,7 @@ df['키'].max()
 df['키'].min()
 df['키'].mean()
 df['키'].count()
+df['item_name'].value_counts()  # 각각의 개수
 df['키'].describe()
 df['키'].info()
 df['SW특기'].count()    # NaN은 카운트되지 않음
